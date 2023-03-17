3008d6488a8fb6be96bb889743ebd1634bcc3f65
KarinaKR00 <kr_karina00@mail.ru>
2023-03-17 19:55:23 +0300

    Первый экран

---
+++ Pet-Yes 2.0/OnboardingModel.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingModel.swift
+//  Pet-Yes 2.0
+//
+//  Created by Карина on 17.03.2023.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+    var id = UUID()
+    var name: String
+    var description: String
+    var imageUrl: String
+    var tag: Int
+    
+    static var simple = Page(name: "Hello", description: "Description", imageUrl: "ScreenOne", tag: 0)
+    
+    static var onboarding: [Page] = [
+        Page(name: "Hello", description: "Description", imageUrl: "ScreenOne", tag: 0),
+        Page(name: "HelloTwo", description: "Description", imageUrl: "ScreenTwo", tag: 1),
+        Page(name: "HelloThree", description: "Description", imageUrl: "ScreenThree", tag: 2)
+    ]
+    
+}
---
+++ Pet-Yes 2.0/OnboardingPage.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingPage.swift
+//  Pet-Yes 2.0
+//
+//  Created by Карина on 17.03.2023.
+//
+
+import SwiftUI
+
+struct OnboardingPage: View {
+    var page: Page
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(page.imageUrl)
+                .resizable()
+                .scaledToFit()
+            Text(page.name)
+                .font(.title)
+                .frame(maxWidth: .infinity)
+            Text(page.description)
+                .font(.subheadline)
+                .frame(maxWidth: .infinity)
+                
+            
+        }
+    }
+}
+
+struct OnboardingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingPage(page: Page.simple)
+    }
+}
+
