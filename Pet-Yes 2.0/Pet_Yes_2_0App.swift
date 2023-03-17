7ffcf95988d1283297763b7d9d1641134911bc36
KarinaKR00 <kr_karina00@mail.ru>
2023-03-17 19:27:05 +0300

    Initial Commit

---
+++ Pet-Yes 2.0/Pet_Yes_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_Yes_2_0App.swift
+//  Pet-Yes 2.0
+//
+//  Created by Карина on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pet_Yes_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
