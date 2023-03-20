d8947bb1573774ba5b0eec7982cc28c0e31f81d3
KarinaKR00 <kr_karina00@mail.ru>
2023-03-20 21:25:04 +0300

    Закончила экран с page control

--- Pet-Yes 2.0/ContentView.swift
+++ Pet-Yes 2.0/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pageIndex = 0
+    private let pages: [Page] = Page.onboarding
+    private let dotAppearence = UIPageControl.appearance()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    Spacer()
+                    OnboardingPage(page: page)
+                    Spacer()
+                    if page == pages.last {
+                        Button("Sign Up", action: goToZero)
+                            .buttonStyle(.bordered)
+                    } else {
+                        Button("Next", action: incrementPage)
+                    }
+                    Spacer()
+                }
+                .tag(page.tag)
+            }
         }
-        .padding()
+        .animation(.easeInOut, value: pageIndex)
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        .onAppear {
+            dotAppearence.currentPageIndicatorTintColor = .black
+            dotAppearence.pageIndicatorTintColor = .gray
+        }
+    }
+    
+    func incrementPage() {
+        pageIndex += 1
+    }
+    
+    func goToZero() {
+        pageIndex = 0
     }
 }
 
--- Pet-Yes 2.0/OnboardingScreen/OnboardingModel.swift
+++ Pet-Yes 2.0/OnboardingScreen/OnboardingModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Page: Identifiable {
+struct Page: Identifiable, Equatable {
     var id = UUID()
     var name: String
     var description: String
